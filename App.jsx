c360f6338965363c863082a560fe38a5ba7a142d
milindcs <shmilind2000@gmail.com>
2026-07-02 16:47:15 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import SiteLayout from '../shared/SiteLayout'
+
+import HomePage from '../views/HomePage/HomePage'
+import AboutPage from '../views/AboutPage/AboutPage'
+import CoursesPage from '../views/CoursesPage/CoursesPage'
+import CourseDetailsPage from '../views/CourseDetailsPage/CourseDetailsPage'
+import EnrollmentPage from '../views/EnrollmentPage/EnrollmentPage'
+import CartPage from '../views/CartPage/CartPage'
+import CheckoutPage from '../views/CheckoutPage/CheckoutPage'
+import ContactPage from '../views/ContactPage/ContactPage'
+import SignupPage from '../views/SignupPage/SignupPage'
+import AllImagesPage from '../views/AllImagesPage/AllImagesPage'
+
+export default function App() {
+  return (
+    <SiteLayout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/courses" element={<CoursesPage />} />
+        <Route path="/course/:course" element={<CourseDetailsPage />} />
+        <Route path="/enrollment" element={<EnrollmentPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/all-images" element={<AllImagesPage />} />
+
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+
+      </Routes>
+
+    </SiteLayout>
+  )
+}
+
+
